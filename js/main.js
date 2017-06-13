22722430e4efd19ee4a85de355d07e3a85dd0d88
Nirmal Rijal <nirmalrijal41@gmail.com>
2017-06-13 09:04:41 +0545

    First commit

---
+++ js/main.js
@@ -0,0 +1,189 @@
+/* particlesJS */
+particlesJS('particles-js',
+
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.3,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+// Smooth scrolling via animate()
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash && window.location.pathname == "/") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// Navigation change on scroll
+$(document).ready(function(){
+  var maxOffset = 300;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= maxOffset) {
+      $('.navbar-default').addClass('navbar-shrink');
+    }
+    else {
+      $('.navbar-default').removeClass('navbar-shrink');
+    }
+  });
+});
+
+$(document).ready(function(){
+  var maxOffset = 300;
+  if ($(window).scrollTop() >= maxOffset) {
+    $('.navbar-default').addClass('navbar-shrink');
+  }
+  else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Async contact form
+$('form[id=contactForm]').submit(function(){
+  $.post($(this).attr('action'), $(this).serialize(), function(res){
+    $('form[id=contactForm] #success').hide();
+    $('form[id=contactForm] #error').hide();
+    if (res.code == "200")
+      $('form[id=contactForm] #success').show();
+    }).fail(function(){
+    $('form[id=contactForm] #success').hide();
+    $('form[id=contactForm] #error').hide();
+    $('form[id=contactForm] #error').show();
+  });
+  return false;
+});
+
+// Contact form validation
+$.validate({
+  modules : 'html5, toggleDisabled'
+});
